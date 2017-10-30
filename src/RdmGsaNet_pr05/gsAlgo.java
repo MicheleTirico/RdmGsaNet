e12be5d97afe687c6843871c38d3b416f1b2eaf5
MicheleTirico <tirico.michele@gmail.com>
2017-10-30 17:45:49 +0100

    new algo loop

--- src/RdmGsaNet_pr05/gsAlgo.java
+++ src/RdmGsaNet_pr05/gsAlgo.java
@@ -1,7 +1,15 @@
 package RdmGsaNet_pr05;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
+
+import scala.collection.parallel.ParIterableLike.Foreach;
+
 
 public interface gsAlgo   {
 	
@@ -12,31 +20,16 @@ public interface gsAlgo   {
 	public enum diffusionType {fick, perimeter, weigth }
 	public enum extType {gsModel, test}
 	
-	//public static Graph GsGraphAlgo = new SingleGraph("GsGraphAlgo");
-	
-	
-	
-	// methods
-
-	
-	/*methods that is used before static method. In gsInit we identify the stream of information 
-	and started parameters
-	*/
-	
-	
-	
-	
-	public static void gsAlgoMain (reactionType r, diffusionType d, extType e) {
+	public static <S> void gsAlgoMain (reactionType r, diffusionType d, extType e) {
 		
 		int stopSim = setupGs.getStopSim();
 		int step ;
 		double Da = setupGs.getDa() ;
 		double Di = setupGs.getDi() ;
-		
-	 
-		
-		// define equations
-	//	double act = 0, inh = 0;
+
+//-----------------------------------------------------------------------------------------------------------		
+//	DEFINE EQUATIONS
+	
 	//	double feed = setupGs.getFeed();
 	//	double kill = setupGs.getKill();
 		
@@ -46,13 +39,114 @@ public interface gsAlgo   {
 	//	double extFeed = gsAlgoExt.gsComputeExt(extType.gsModel, morphogen.activator,feed, kill, act, inh ) ;
 	//	double extKill = gsAlgoExt.gsComputeExt(extType.gsModel, morphogen.inhibitor,feed, kill, act, inh ) ;
 
-		// loop
+//-----------------------------------------------------------------------------------------------------------		
+//	LOOP SIMULATION	
 		
 		Graph graph = setupGs.getGraph( setupGs.GsGraph );
 		for ( step = 1 ; step <= stopSim ;  step++  ) {
-
+			
 			System.out.println(step);
 			
+			
+			// map0 (key, double, double) = id node, act, inh
+			
+			// CREATE MAP0 AND MAP1
+			
+			// mapMor0
+			Map<String, ArrayList> mapMorp0 = new HashMap<String, ArrayList>();
+			
+			// mapMorp1
+			Map<String, ArrayList> mapMorp1 = new HashMap<String, ArrayList>();
+			
+			// SET MORP IN MAP0
+			for ( Node n : graph.getEachNode() ) {
+				
+
+				// set act and inh in map0
+				ArrayList<Double> ArListMorp0 = new ArrayList<Double>() ;
+			
+				ArListMorp0.add( (double) 0 );
+				ArListMorp0.add( (double) 0 );
+			
+				ArListMorp0.set(0, (double) n.getAttribute( "GsAct" )) ;
+				ArListMorp0.set(1, (double) n.getAttribute( "GsInh" )) ;
+				
+				
+				mapMorp0.put(n.getId(), ArListMorp0 );
+				
+			}
+			
+			// LOOP SIMULATION
+			
+			
+			for ( Node n : graph.getEachNode() ) {
+		
+				ArrayList ListMorp = mapMorp0.get(n.getId()) ; 
+				
+				double act0 = (double) ListMorp.get(0);
+				double inh0 = (double) ListMorp.get(1);
+				
+				double reaction = gsAlgoReaction.gsComputeReaction(reactionType.ai2, act0, inh0);
+				
+//				double diffusionAct = gsAlgoDiffusion.gsComputeDiffusion(diffusionType.fick, Da);
+//				double diffusionInh = gsAlgoDiffusion.gsComputeDiffusion(diffusionType.fick, Di);
+//				double extFeed = gsAlgoExt.gsComputeExt(extType.gsModel, morphogen.activator,feed, kill, act, inh ) ;
+//				double extKill = gsAlgoExt.gsComputeExt(extType.gsModel, morphogen.inhibitor,feed, kill, act, inh ) ;
+				
+				double act1 = act0  + reaction;
+				double inh1 = inh0  - reaction;
+				
+//				double act1 = act0 + diffusionAct + reaction;
+//				double inh1 = inh0 + diffusionInh - reaction;
+				
+				// set act and inh in map1
+				ArrayList<Double> ArListMorp1 = new ArrayList<Double>() ;
+			
+				ArListMorp1.add( act1);
+				ArListMorp1.add( inh1);
+			
+				mapMorp1.put(n.getId(), ArListMorp1 );				
+				
+			}
+			// per ogni key di map0, sostituire il value (list) di map1
+			for (Entry<String, ArrayList> entry : mapMorp0.entrySet()) {
+				
+				ArrayList<Double> list0 = new ArrayList<Double>() ;
+				ArrayList<Double> list1 = new ArrayList<Double>() ;
+				
+				double act = 1 ; 
+				double inh = 0 ; 
+				list0.add(act);
+				list0.add(inh);
+				
+				mapMorp0.put(entry.getKey(), list0);
+				
+				
+				
+			}
+			
+			
+		
+			
+			System.out.println(mapMorp1);
+			
+			// create map0  and map1
+			
+			// set morp in map0
+			
+			// loop execute gsalgo (for each node )
+				
+				// act-inh = act-inh(in map) + d + r + e 
+			
+				// set act-inh in map1
+			
+			// loop set act-inh in map1 (for each node )
+			
+		}
+	}
+	
+}
+			/*
 			for(  Node n : graph.getEachNode () ) {
 				
 				double OldAct, OldInh;
@@ -67,8 +161,10 @@ public interface gsAlgo   {
 				
 				double diffusionAct = gsAlgoDiffusion.gsComputeDiffusion(diffusionType.fick, Da);
 				double diffusionInh = gsAlgoDiffusion.gsComputeDiffusion(diffusionType.fick, Di);
+				double extFeed = gsAlgoExt.gsComputeExt(extType.gsModel, morphogen.activator,feed, kill, act, inh ) ;
+				double extKill = gsAlgoExt.gsComputeExt(extType.gsModel, morphogen.inhibitor,feed, kill, act, inh ) ;
 				
-				
+			
 				
 				act = act + diffusionAct + reaction;
 				inh = inh + diffusionInh - reaction;
@@ -78,10 +174,6 @@ public interface gsAlgo   {
 				
 				System.out.println( act ) ;
 				System.out.println( inh );
-			
-			
-			
-			
 			
 			
 			}}}}
